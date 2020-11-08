d1b6c67cb6536b0fb445187958321890f78358de
Joseph Lunine <jel99@drexel.edu>
2020-11-07 23:43:23 -0500

    updated to prod service endpoint

--- src/repositories/repository.js
+++ src/repositories/repository.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // You can use your own logic to set your local or production domain
-// const baseDomain = "http://affirmation-photo-service:10000";
-const baseDomain = "http://127.0.0.1:5000";
+const baseDomain = "http://affirmations-photo-service:10000";
+// const baseDomain = "http://127.0.0.1:5000";
 // The base URL is empty this time due we are using the jsonplaceholder API
 const baseURL = `${baseDomain}`;
 
