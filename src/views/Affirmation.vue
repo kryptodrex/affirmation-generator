7c7e62ea2250151d5aed05e6efe6edbb62e8c74e
Joseph Lunine <jel99@drexel.edu>
2020-11-08 18:50:41 -0500

    updated how photo response is handled
    Added ability to pull back other entities, also handle if no photo comes back from search

--- src/views/Affirmation.vue
+++ src/views/Affirmation.vue
@@ -3,23 +3,34 @@
     <div id="loading" class="loading" v-if="isLoading">
         <p class="loader">Loading...</p>
     </div>
+
     <div class="affirmationBody" v-if="!isLoading">
-      <img id="unsplashPhoto" :src="photoData.url" :alt="photoData.altText">
-      <p class="photoCreds">
-        Photo by <a target="_blank" :href="photoData.creatorProfile">{{ photoData.creatorName }}</a> on <a href="https://unsplash.com" target="_blank">Unsplash</a>.
-      </p>
-      <p class="affirmationText">{{ affirmationData.affirmation }}.</p>
+        <div class="affirmationPhoto" v-if="foundResult">
+          <img id="unsplashPhoto" :src="photoData.url" :alt="photoData.altText">
+          <p class="photoCreds">
+            Photo by <a target="_blank" :href="photoData.creatorProfile">{{ photoData.creatorName }}</a> on <a href="https://unsplash.com" target="_blank">Unsplash</a>.
+          </p>
+        </div>
+        <p class="noPhoto" v-if="!foundResult">No photo found for affirmation :(</p>
+
+        <div class="affirmations">
+          <p class="affirmationText">{{ affirmationData.affirmation }}.</p>
+          <!-- <span v-if="affirmationData.other_entities.length != 0">
+            Other entities: 
+            <span v-for="entity in affirmationData.other_entities" :key="entity">{{ entity }}</span>
+          </span> -->
+        </div>
+        
 
       <div class="actionButtons">
         <a href="/affirmation">
            <Button text="Generate new affirmation" size="medium" />
         </a>
-        <div v-on:click="isCopied = true">
+        <div v-on:click="isCopied = true" v-if="foundResult">
           <Button id="copyBtn" text="Copy share link" size="medium" v-clipboard="shareUrl"/>
         </div>
       </div>
       <p v-if="isCopied" class="copied"> Link copied! </p>
-      
     </div>
   </div>
 </template>
@@ -44,7 +55,8 @@ export default {
       photoId: "",
       photoData: null,
       shareUrl: "",
-      isCopied: false
+      isCopied: false,
+      foundResult: false
     };
   },
   created () {
@@ -58,36 +70,51 @@ export default {
   methods: {
     async fetch () {
       this.isLoading = true;
+
+      // Get affirmation data from service, along with entity
       var { data } = await affirm.getAffirmation(this.reloadToken);
-      this.affirmationData = {
-        "affirmation": data.affirmation,
-        "reloadToken": data.reloadToken,
-        "entity": data.entity
-      };
+      this.affirmationData = data;
 
       var textToSearch;
-      if (data.entity != "") {
-        textToSearch = data.entity;
+      if (data.top_entity != "") {
+        textToSearch = data.top_entity;
       } else {
         textToSearch = data.affirmation;
       }
 
+      // Check if photo id was passed in shareToken
+      // if it was, use that to get photo; if not, search for new based on entity
       if (this.photoId != ""){
         var { data } = await photo.getPhotoById(this.photoId,'regular');
       } else {
         var { data } = await photo.getPhotos(textToSearch,'regular');
       }
-      this.photoData = {
-        'url': data.urls.direct,
-        'altText': data.alt_description,
-        'photoId': data.photo_id,
-        'creatorName': data.user.name,
-        'creatorProfile': data.user.profile
-      };
 
+      if (data.foundPhoto != 0) {
+        this.foundResult = true;
+        this.photoData = {
+          'url': data.urls.direct,
+          'altText': data.alt_description,
+          'photoId': data.photo_id,
+          'creatorName': data.user.name,
+          'creatorProfile': data.user.profile
+        };
+      } else {
+        this.foundResult = false;
+        this.photoData = {
+          'url': "",
+          'altText': "",
+          'photoId': "",
+          'creatorName': "",
+          'creatorProfile': ""
+        };
+      }
+      
+      // Get current url to use for sharing
       var currentUrl = window.location.href;
       console.log(currentUrl);
       
+      // If there is already a share token, take current url; if not, create new token
       if (this.sharing != null) {
         this.shareUrl = currentUrl;
       } else {
@@ -123,20 +150,27 @@ img {
 
 .actionButtons {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 }
 
 .copied {
-  color: rgb(128, 68, 163);
   font-size: 80%;
+}
+
+.noPhoto {
+  font-size: 130%;
 }
 
 @media only screen and (min-width: 1024px) {
 
   img {
     max-height: 42rem;
+  }
+
+  .actionButtons {
+    flex-direction: row;
   }
 
 }
